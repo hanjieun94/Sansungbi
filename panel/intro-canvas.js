23a751d4d87c390fecb65af3e0cb0bea1bb95b90
jieun <k.paper94@gmail.com>
2023-01-11 22:17:05 +0900

    11day2

---
+++ panel/intro-canvas.js
@@ -0,0 +1,13 @@
+class Introcavas{
+    constructor(){
+
+        //canvas사용
+        this.dom = document.querySelector("intro-canvas");
+        /** @typeof {CanvasRenderingContext2D} */
+
+
+
+    }
+
+
+}
